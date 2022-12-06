a172a3dbd03681c38b8c1c50bc3e07f8b914d141
Steve Gossip <stephengossip@gmail.com>
2022-12-06 18:03:18 +0000

    second test passing. A player has 6 lives to begin with.

--- player.rb
+++ player.rb
@@ -1,6 +1,7 @@
 class Player
-  attr_reader :name
+  attr_reader :name, :lives
   def initialize(name)
     @name = name
+    @lives = 6
   end
 end
--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -7,4 +7,9 @@ class TestPlayer < MiniTest::Test
     player1 = Player.new("Stephen")
     assert_equal("Stephen", player1.name)
   end
+
+  def test_a_player_has_6_lives
+    player1 = Player.new("Stephen")
+    assert_equal(6, player1.lives)
+  end
 end
