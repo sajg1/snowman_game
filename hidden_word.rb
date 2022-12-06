082e30cc9f67f55efa9acf96f31954fb1b0a7467
Steve Gossip <stephengossip@gmail.com>
2022-12-06 22:08:27 +0000

    passing test for revealing only letters that have been guessed and the rest being represented as *.

--- hidden_word.rb
+++ hidden_word.rb
@@ -5,15 +5,28 @@ class HiddenWord
   end
 
   def reveal(guessed_letters=[])
+    @hidden = ""
+    count = 0
     if guessed_letters.length > 0
-      pass
+      for letter in @word.split("") do
+        for guess in guessed_letters do
+          if letter == guess
+            @hidden += letter
+          end
+        end
+        if count == @hidden.length
+          @hidden += "*"
+        end
+        count += 1
+      end
     else
       i=0
       while i < @word.length
         @hidden += "*"
         i += 1
       end
-    return @hidden
     end
+    return @hidden
   end
+
 end
--- specs/hidden_word_spec.rb
+++ specs/hidden_word_spec.rb
@@ -7,4 +7,14 @@ class TestHiddenWord < MiniTest::Test
     word = HiddenWord.new("hello")
     assert_equal("*****", word.reveal())
   end
+
+  def test_letter_can_be_revealed
+    word = HiddenWord.new("hello")
+    assert_equal("h****", word.reveal(["h"]))
+    assert_equal("*e***", word.reveal(["e"]))
+    assert_equal("**ll*", word.reveal(["l"]))
+    assert_equal("****o", word.reveal(["o"]))
+    assert_equal("h*ll*", word.reveal(["h","l"]))
+    assert_equal("*e**o", word.reveal(["o","e", "p", "s", "i"]))
+  end
 end
